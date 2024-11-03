e01250437e4048d1443d2c84557b0535756ca1dc
matushalak <matus.halak@gmail.com>
2024-11-03 22:49:28 +0100

    parts A,B solved!

--- assignment1/ass1.py
+++ assignment1/ass1.py
@@ -1,1 +1,58 @@
-# First part of the first assignment
+from numpy import zeros, argmax
+from pandas import DataFrame
+
+def basic_inventory_dp(T_dim, X_dim, A, P):
+    DP = zeros((X_dim, T_dim)) # initialize with zeros 
+    POLICY = zeros((X_dim,T_dim-1))
+
+    # Expected immediate reward = prob of action * value of action
+    # in ABC reward is independent of state
+    ER = [p*a for p,a in zip(P,A)]
+
+    # Value function
+    # Bellman equation:
+        # V(x,t) = max(a E {50,100,200}) (a * P(a) + P(a) * V(x-1,t+1) + (1-P(a)) * V(x, t+1))
+    # go backwards filling the table from the ...<-498th<-499th<-500th column
+    # 501st column is all just zeros (no value after time horizon)
+    for t in range(T_dim-2, -1, -1):
+        assert all(DP[0,:] == 0)
+        # 0th row - inventory == 0 is untouched, that is the boundary condition from which
+        # the divergence in values across inventory levels emerges
+        for x in range(1,X_dim):
+            possible_actions = []
+            for ai, a in enumerate(A):
+                immediate_reward = ER[ai]
+                taken_action = P[ai] * DP[x - 1, t + 1]
+                untaken_action = (1 - P[ai]) * DP[x, t + 1]
+                possible_actions.append(immediate_reward + taken_action + untaken_action)
+            
+            # best_action
+            best_action = argmax(possible_actions)
+            DP[x,t] = possible_actions[best_action]
+            POLICY[x,t] = A[best_action]
+
+    # Maximum expected revenue
+    print(max(DP[100,0]))
+
+    DP_frame = DataFrame(DP, columns = [f't={t}' for t in range(T_dim)]).to_csv('Part1matrix.csv')
+    POLICY_frame = DataFrame(POLICY, columns = [f't={t}' for t in range(T_dim-1)]).to_csv('Part1policy.csv')
+
+    return {'dp_matrix':(DP, DP_frame),
+            'policy':(POLICY, POLICY_frame)}
+
+if __name__ == '__main__':
+    # GOAL: MAX SALES (max expected cumulative rewards)
+    # Time (T): 500:0 decisions left in finite time horizon
+    # State (Xt): Invenstory left at time t - 0:100
+    #Dimensions of DP matrix
+    T_dim, X_dim = 501, 101
+
+    # Actions (A): Change price to 200 | 100 | 50
+    # and corresponding immediate reward
+    A = [50,100,200]
+
+    # probability of sale with given price
+    P = [0.8, 0.5, 0.1]
+
+    # AB parts of the assignment
+    ab_results = basic_inventory_dp(T_dim, X_dim, A, P)
