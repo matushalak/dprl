48babeb8b0d46a1ee6754f5be231b09ed660bfb7
matushalak <matus.halak@gmail.com>
2024-11-03 23:25:05 +0100

    optimal policy graph done!

--- assignment1/ass1.py
+++ assignment1/ass1.py
@@ -1,7 +1,11 @@
-from numpy import zeros, argmax
+from numpy import zeros, argmax, ndarray, meshgrid
 from pandas import DataFrame
+import matplotlib.pyplot as plt
 
-def basic_inventory_dp(T_dim, X_dim, A, P):
+def basic_inventory_dp(T_dim:int, X_dim:int, A:list, P:list
+                       )->dict[str:tuple[ndarray, DataFrame], 
+                               str:tuple[ndarray, DataFrame]]:
+    
     DP = zeros((X_dim, T_dim)) # initialize with zeros 
     POLICY = zeros((X_dim,T_dim-1))
 
@@ -15,7 +19,7 @@ def basic_inventory_dp(T_dim, X_dim, A, P):
     # go backwards filling the table from the ...<-498th<-499th<-500th column
     # 501st column is all just zeros (no value after time horizon)
     for t in range(T_dim-2, -1, -1):
-        assert all(DP[0,:] == 0)
+        assert all(DP[0,:] == 0) # make sure that when inventory 0, value always 0
         # 0th row - inventory == 0 is untouched, that is the boundary condition from which
         # the divergence in values across inventory levels emerges
         for x in range(1,X_dim):
@@ -32,13 +36,25 @@ def basic_inventory_dp(T_dim, X_dim, A, P):
             POLICY[x,t] = A[best_action]
 
     # Maximum expected revenue
-    print(max(DP[100,0]))
+    print(DP[100,0])
 
     DP_frame = DataFrame(DP, columns = [f't={t}' for t in range(T_dim)]).to_csv('Part1matrix.csv')
     POLICY_frame = DataFrame(POLICY, columns = [f't={t}' for t in range(T_dim-1)]).to_csv('Part1policy.csv')
 
     return {'dp_matrix':(DP, DP_frame),
             'policy':(POLICY, POLICY_frame)}
+
+def plot_policy(Td:int, Xd:int, pol:ndarray)->None:
+    X, Y = meshgrid(list(range(Td-1)),
+                    list(range(Xd)))
+    
+    policy_plot = plt.contourf(X,Y,pol, levels = 1)
+    plt.colorbar(policy_plot, label = 'Optimal policy (t, x)')
+    plt.xlabel('Time period (t)')
+    plt.ylabel('Remaining inventory (x)')
+    plt.tight_layout()
+    plt.savefig('optimal_policy_partB.png')
+    plt.show()
 
 if __name__ == '__main__':
     # GOAL: MAX SALES (max expected cumulative rewards)
@@ -56,3 +72,4 @@ if __name__ == '__main__':
 
     # AB parts of the assignment
     ab_results = basic_inventory_dp(T_dim, X_dim, A, P)
+    plot_policy(T_dim, X_dim, ab_results['policy'][0])
