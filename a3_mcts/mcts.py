e3839a00a3c7f69671197bd9c389f132bdfd9215
matushalak <matus.halak@gmail.com>
2024-11-26 14:23:20 +0100

    basic functions done, Tree class and MCTS to go!

--- a3_mcts/mcts.py
+++ a3_mcts/mcts.py
@@ -4,21 +4,27 @@
 Connect Four is a board-size
 6 x 7, representation B \in \R^{6 X 7}
 '''
-from numpy import ndarray, unique, fliplr
+from numpy import ndarray, unique, fliplr, array, where, zeros
 from numpy.random import randint
 from string import ascii_letters as ascl
 import argparse
+from collections import defaultdict
 
-def print_board(B:ndarray, symbols:dict[int:str, int:str]):
+def string_board(B:ndarray, symbols:dict[int:str, int:str] = ['⚽️','🍎']
+                 ) -> str:
     # printed board with symbols
     header = [' | '+l for l in ascl[:B.shape[1]]]
     PrB = ' '+ ''.join(header)+ ' |  ' +'\n'
+    string_board = ''
     for i, row in enumerate(B):
+        string_board += ''.join([str(n) for n in row]) #hashable board representation
         symb = [f'{symbols[n]} |' for n in row]
         PrB += str(i)+ ' |' + ''.join(symb) + ' ' + str(i) + '\n'
     PrB += len(' '+ ''.join(header)+ ' |  ') * '-' + '\n'
     PrB += ' '+ ''.join(header)+ ' |  '
     print(PrB)
+    return string_board #hashable board representation
+
 
 def evaluate_board(B:ndarray):
     # only loop through pivots and look at 4x4 slices of B
@@ -42,7 +48,61 @@ def evaluate_board(B:ndarray):
                 # breakpoint()
                 winner = sub_res[0]
                 return winner
-    return winner # None if no winner (no connect 4)
+    if 0 not in B:
+        return 0 # draw if no free pieces left
+    else:
+        return winner # None if no winner (no connect 4), but still pieces left
+
+
+def available_moves(B:ndarray):
+    return [i for i,c in enumerate(B.T) if 0 in c] # transpose for columns
+
+
+def moves(player:int, B:ndarray, possible_moves:list[int]) -> list[ndarray]:
+    assert player in (1,2), 'Needs to be either player 1 or player 2!!!'
+    resulting_states = []
+    for m in possible_moves:
+        # find last row in which 0 is still present
+        B_new = B.copy()[where(B[:,m] == 0)[0][-1], m]
+        B_new[where(B[:,m] == 0)[0][-1], m] = player
+        resulting_states.append(B_new)
+    return resulting_states
+
+# TODO: finish
+class TreeNode():
+    def __init__(self, 
+                 B:ndarray, 
+                 player = 2):
+        self.player = player
+        self.amoves = available_moves(B)
+
+        self.root = B # root
+        self.children = moves(self.player, self.STATE, self.amoves) # children
+
+# TODO: finish
+def MCTS(startB:ndarray):
+    # So that  no error thrown if new board state indexed
+    state_node_map = defaultdict(None)
+    sB = string_board(startB)
+    B = startB
+    while True:
+        # Player turn
+        # store possible board configurations in state_node_map
+        state_node_map[sB] = TreeNode(B)
+        # Oponent turn
+        # results in B & sB
+        if state_node_map[sB]:
+            state = state_node_map[sB]
+        else:
+            state_node_map[sB] = TreeNode(B)
+
+        pass
+
+
+# State = current Board config
+def build_tree(STATE:ndarray):
+    pass
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -54,10 +114,23 @@ if __name__ == '__main__':
     symbols = [' '+s if s.isalnum() else s for s in args.sym]
     d = {0:'  ', 1:symbols[0], 2:symbols[1]}
     # Board
-    # B = np.zeros((6,7), dtype=int)
-    B = randint(0, 3, (6,7), dtype=int) # for testing & evaluation
-    print_board(B, d)
+    # hardcoded for assignment
+    hardcodedB = array([[1, 1, 1, 2, 0, 2, 0],
+                        [1, 2, 2, 2, 0, 1, 0],
+                        [2, 1, 1, 1, 0, 2, 0],
+                        [1, 2, 2, 2, 0, 1, 0],
+                        [2, 2, 2, 1, 0, 1, 0],
+                        [1, 1, 2, 1, 0, 2, 0]])
+    zeroB = zeros((6,7), dtype=int) # for the real game
+
+    # B = randint(1, 3, (6,7), dtype=int) # for testing & evaluation
+    # B = zeroB
+    B = hardcodedB
+
+    sB = string_board(B, d)
+    # winner = evaluate_board(B)
     winner = evaluate_board(B)
+    print(available_moves(B))
     if winner:
         print(f'WinnerID:{winner} -> {d[winner]}')
     else:
