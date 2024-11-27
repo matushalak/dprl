adb5fa6e36b497ee86fe27f852bc08e474bc62ec
matushalak <matus.halak@gmail.com>
2024-11-28 00:16:11 +0100

    MCTS progress

--- a3_mcts/mcts.py
+++ a3_mcts/mcts.py
@@ -4,7 +4,7 @@
 Connect Four is a board-size
 6 x 7, representation B \in \R^{6 X 7}
 '''
-from numpy import ndarray, unique, fliplr, array, where, zeros
+from numpy import ndarray, unique, fliplr, array, where, zeros, mean, argmax, log
 from numpy.random import randint, choice
 from string import ascii_letters as ascl
 import argparse
@@ -55,25 +55,44 @@ def evaluate_board(B:ndarray):
     else:
         return winner # None if no winner (no connect 4), but still pieces left
 
-
+# this tree Class does the 1/3 of heavy lifting
 class TreeNode():
     def __init__(self, 
                  B:ndarray,
                  parent = None): # that's how you identify root node!!!
         self.player = self.turn(B)
-        self.amoves = self.available_moves(B)
+        # Actions := Free fields / available moves
+        self.actions = self.available_moves(B)
         self.parent = parent
+        self.parent_move = None # for root
+
+        self.board = B # root
+        self.strB = ''.join([str(n) for n in B.ravel()]) # hashable representation
 
         # only increase w back-up
         self.depth = 0 if not parent else parent.depth + 1
-        self.Q = 0
-        self.visits = 0
+        # only for ultimate leaf nodes of MCT -1 / 0 / 1
+        self.reward = 0
+        self.state_visits = 0 # #times this state visited
 
-        self.board = B # root
-        self.strB = ''.join([str(n) for n in self.B.ravel()])
+        # Q-values for each (current_state, possible_action) pair
+        self.Qs = dict.fromkeys(self.actions, zeros(len(self.actions)))
+        # Visits for each (state, action) pair
+        self.visits = dict.fromkeys(self.actions, zeros(len(self.actions)))
         # children are Treenodes themselves, self is parent of its children
-        # Qs are Q-values associated with each possible child
-        self.children, self.Qs = self.moves(self.player, self.board, self.amoves, parent = self) 
+        self.children = dict.fromkeys(self.actions, None) # added on a need-to-add basis
+
+    # Only Add child when needed, no need to generate self.children every time a node is accessed
+    def add_child(self, action:int) -> 'TreeNode':
+        assert self.player in (1,2), 'Needs to be either player 1 or player 2!!!'
+        B_new:ndarray = self.board.copy()
+        B_new[where(B_new[:,action] == 0)[0][-1], 
+              action] = self.player
+        # child node
+        ChildNode = TreeNode(B_new, self) # self is a Parent
+        ChildNode.parent_move = action # can call this up later
+        self.children[action] = ChildNode
+        return ChildNode # for use outside
 
     def available_moves(self, B:ndarray):
         return [i for i,c in enumerate(B.T) if 0 in c] # transpose for columns
@@ -86,25 +105,14 @@ class TreeNode():
         else:
             return 1 # if same then 1st players turn 
 
-    def moves(self, player:int, B:ndarray, possible_moves:list[int], parent:'TreeNode'
-              ) -> list['TreeNode']:
-        assert player in (1,2), 'Needs to be either player 1 or player 2!!!'
-        children = []
-        Qs = dict() # Q-values for each possible action a_j(children) from state x_i (parent)
-        for m in possible_moves:
-            # find last row in which 0 is still present
-            B_new = B.copy()
-            B_new[where(B_new[:,m] == 0)[0][-1], m] = player
-            strB_new = ''.join([str(n) for n in B_new.ravel()])
-            Qs[strB_new] = 0 # initiate with 0 Q-values
-            # child node
-            child_node = TreeNode(B_new, parent)
-            children.append(child_node)
-        return children, Qs
-
     # Returns -1 (loss), 0(draw), +1 (win)
     def rollout(self)->int:
         rollB = self.board.copy()
+        # end of the game, terminal node
+        if self.available_moves(rollB) == []:
+            self.reward = evaluate_board(rollB)
+            return self.reward
+
         while True:
             winner = evaluate_board(rollB)
             if winner is not None:
@@ -127,8 +135,8 @@ class TreeNode():
 
 
 # TODO: finish
-# TODO, multiple rollouts / node - especially EARLY in the game
-def MCTS(startB:ndarray):
+# this MCTS function does 2/3 of heavy lifting
+def MCTS(startB:ndarray, c:float = 2**0.5):
     '''
     Main function performing Monte Carlo Tree Search Algorithm 
     with Upper Confidence Trees (UCT) & min-max algorithm to consider playing optimal oponent
@@ -138,16 +146,18 @@ def MCTS(startB:ndarray):
         (apply on each previously seen node, balances exploration & exploitation)
         - for Node.depth == 0, need to random rollout on all possible moves 
         (ideally multiple times, decreasing as a function of Node depth)
-    2) Expansion
+    2) Expansion:
         - when leaf Node reached add child(ren) of this node
-    3) Simulation
+    3) Simulation:
         - random rollout from one (random) child of leaf Node until end
-    4) Back-up
+    4) Back-up:
+    XXX Q-VAlUES NOT ASSOCIAteD w CHILD / STATE !!!! ONLY w STATE-ACTION PAIR!!!! XXX
         - update Q-value & visits for Nodes in Selection & expansion
-        Q(state, action) = ∑_x' [#x' reached from (x,a)] . (
-        immediate reward (0) + max/min{based on p1/p2}_a' Q(x', a')) // # a taken in state x
+            -> for deterministic games Go, Chess, TicTacToe, Connect4, we dont need to keep track of 
+            different possible states reached by taking action: always 1 Action --> 1 State
+        Q(state, action) = immediate reward + max/min{based on p1/p2}_a' Q(x', a'))
 
-        Q(state, action) = All maximal future rewards, possibly achieved by action weighed by their frequency // frequency of action in given state
+        Q(state, action) = Sum of rewards achieved by action weighed by their frequency // frequency of action in given state
             => Gives an estimate of Value / Action in a state
         - if oponent actions kept negative: use max(p1)-min(p2) approach! 
             -> max reward for worst-case scenario
@@ -156,30 +166,58 @@ def MCTS(startB:ndarray):
     # stores hashable reference to every possible state (which has its depth, children, etc.)
     state_node_map = defaultdict(lambda: None)
     sB = string_board(startB)
-    B = startB
-    while True:
-        # turn determined within treenode_class
-        # store possible board configurations in state_node_map
-        if state_node_map[sB]:
-            state = state_node_map[sB]
-        else:
-            state_node_map[sB] = TreeNode(B)
-            state = state_node_map[sB]
+    # store possible board configurations in state_node_map
+    StartState:TreeNode = TreeNode(startB)
+    state_node_map[sB] = StartState
 
-        if state.depth == 0:
-            for ch in state.children:
-                r = ...
-                node.parent = ch
-                # Backup
-                while node.parent is not None:
-                    ...
+    # Starting case - expand all children, and use average of 5 rollout rewards for initial Q-values
+    for action in StartState.actions:
+        # if unexplored action from root node
+        if StartState.visits[action] == 0:
+            Ch:TreeNode = StartState.add_child(action)
+            state_node_map[Ch.strB] = Ch
+            # random rollout reward from (x,a)->initial estimate of Q(x,a) == here x.Q[a] 
+            StartState.Qs[action] += mean([Ch.rollout() for _ in 5]) # average of 5 random rollouts, for more accurate initial Q-vals
+            # n(x, a) == here x.n[a]
+            StartState.visits[action] += 1
+
+            # N(x) == x.N
+            Ch.state_visits += 1
+            StartState.state_visits += 1
+
+    # in MCTS - AFTER Backpropagation: Always start again at root Node!
+    State : TreeNode = StartState
+    while True:
+        # Terminal node, no point in looking at its actions
+        if State.reward != 0:
+            break
+        
+        # UCB(x, a) = argmax_a{ Q(x,a) + c. √[ln(x.state_visits) / (x.visits[action])]
+        UCB = argmax([State.Qs[a] + c * (log(State.state_visits) / State.visits[action])**0.5
+                      for a in State.actions])
+        
+        # TODO complete this logic -> what to do when leaf node(add child) vs not leaf node (continue into next state in while loop)
+        # Continue onto child with highest UCB
+        State = State.children[State.actions[UCB]]
+
 
         breakpoint()
 
-        state_node_map[sB] = TreeNode(B)
-        # Oponent turn
-        # results in B & sB
-        pass
+        # for Ch in State.children:
+        #     r = Ch.rollout
+        #     Node = Ch
+        #     # Backup up to root node, Q-value of root note not really meaningful
+        #     while Node.parent is not None:
+        #         Node.visits += 1
+                    
+        # else:
+        #     sB = Node.strB
+        #     B = Node.board
+        #     if state_node_map[sB] is None:
+        #         state_node_map[sB] = TreeNode(B)
+        #         State = state_node_map[sB]
+
+        # pass T
 
 
 def parse_args():
